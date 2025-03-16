dcc4a79191cee8f0bf626c578bdcf11a4e1c39a7
tontoko <hirakitomohiko@gmail.com>
2025-03-17 02:38:07 +0900

    jest.setup.tsのテストクライアントのモック設定を有効化し、TestPrismaClientの初期化を修正

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,11 +3,11 @@ import { TestPrismaClient } from './src/prisma/testPrismaClient.service';
 jestPrisma.initializeClient(new TestPrismaClient());
 
 beforeAll(() => {
-  // jest.mock('./src/prisma/testPrismaClient.service', () => {
-  //   return {
-  //     TestPrismaClient: jest.fn().mockImplementation(() => {
-  //       return jestPrisma.client;
-  //     }),
-  //   };
-  // });
+  jest.mock('./src/prisma/testPrismaClient.service', () => {
+    return {
+      TestPrismaClient: jest.fn().mockImplementation(() => {
+        return jestPrisma.client;
+      }),
+    };
+  });
 });
